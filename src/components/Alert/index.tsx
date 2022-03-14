a1f2fdbaae7cdd86558a876eb9a369786d98ce4a
Alexandr <alexryapolov36@gmail.com>
2022-03-14 21:56:53 +0400

    fixed position alert

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -19,30 +19,32 @@ export class Alert extends Component<IProps, {}> {
   }
 
   render(): ReactNode {
-    let alertClasses = ['alert'];
+    let alertClasses = ['alert__content'];
     let icon: ReactElement;
     switch (this.props.alert.type) {
       case AlertType.success:
-        alertClasses.push('alert_success');
+        alertClasses.push('alert__content_success');
         icon = <IoCheckmarkCircleOutline />;
         break;
       case AlertType.info:
-        alertClasses.push('alert_info');
+        alertClasses.push('alert__content_info');
         icon = <IoInformationCircleOutline />;
         break;
       case AlertType.warning:
-        alertClasses.push('alert_warning');
+        alertClasses.push('alert__content_warning');
         icon = <IoWarningOutline />;
         break;
     }
-    if (this.props.alert.show) alertClasses.push('alert_show');
+    if (this.props.alert.show) alertClasses.push('alert__content_show');
 
     return (
-      <div className={alertClasses.join(' ')}>
-        <p className="alert__text">
-          {icon}
-          {this.props.alert.text}
-        </p>
+      <div className="alert">
+        <div className={alertClasses.join(' ')}>
+          <p className="alert__text">
+            {icon}
+            {this.props.alert.text}
+          </p>
+        </div>
       </div>
     );
   }
