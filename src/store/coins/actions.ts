f53b77febd8daabe9a75389c6e4a921c3254d319
Alexandr <alexryapolov36@gmail.com>
2022-04-13 22:00:38 +0400

    move api request to commons module

--- src/store/coins/actions.ts
+++ src/store/coins/actions.ts
@@ -1,31 +1,23 @@
-import axios from 'axios';
 import { Dispatch } from 'redux';
-import { normalizeNumber } from '../../utils/commons';
-import { API_KEY, BASE_URL, CURRENCY } from '../../utils/constants';
+import { getPrice, normalizeNumber } from '../../utils/commons';
 import { CoinActions } from '../../utils/enums';
 import { ICoin } from '../../utils/interfaces';
 import { RootAction } from './reducer';
 
 export const addCoin =
   (name: string) => async (dispatch: Dispatch<RootAction>) => {
-    try {
-      const url = `${BASE_URL}?fsym=${name}&tsyms=${CURRENCY}&api_key=${API_KEY}`;
-      const response = await axios.get(url);
+    const result = await getPrice(name);
 
-      if (response.data?.Response === 'Error')
-        throw new Error(`Sorry! Coin "${name}" was not found`);
-
-      const currentPrice = normalizeNumber(response.data[CURRENCY]);
-
+    if (typeof result === 'string') {
+      dispatch({ type: CoinActions.GET_COINS_FAILURE, payload: result });
+    } else if (typeof result === 'number') {
       const coin = {
         name,
-        price: currentPrice,
+        price: result,
         diff: 0,
       };
 
       dispatch({ type: CoinActions.ADD_COIN, payload: coin });
-    } catch (err) {
-      console.log(err);
     }
   };
 
@@ -36,27 +28,22 @@ export const RemoveCoin = (coin: ICoin) => ({
 
 export const updateCoinPrice =
   (coin: ICoin) => async (dispatch: Dispatch<RootAction>) => {
-    try {
-      const url = `${BASE_URL}?fsym=${coin.name}&tsyms=${CURRENCY}&api_key=${API_KEY}`;
-      const response = await axios.get(url);
+    const result = await getPrice(coin.name);
 
-      if (response.data?.Response === 'Error')
-        throw new Error(`Sorry! Coin "${coin.name}" was not found`);
+    console.log(`check updates ${coin.name}`);
 
-      const currentPrice = normalizeNumber(response.data[CURRENCY]);
-
-      if (coin.price !== currentPrice) {
+    if (typeof result === 'string') {
+      dispatch({ type: CoinActions.GET_COINS_FAILURE, payload: result });
+    } else if (typeof result === 'number') {
+      if (coin.price !== result) {
         const updatedCoin = {
           name: coin.name,
-          price: currentPrice,
+          price: result,
           diff: 0,
         };
-        if (coin.price)
-          updatedCoin.diff = normalizeNumber(currentPrice - coin.price);
+        if (coin.price) updatedCoin.diff = normalizeNumber(result - coin.price);
 
         dispatch({ type: CoinActions.UPDATE_COIN, payload: updatedCoin });
       }
-    } catch (err) {
-      console.log(err);
     }
   };
--- src/utils/commons.tsx
+++ src/utils/commons.tsx
@@ -1,6 +1,25 @@
+import axios from 'axios';
+import { API_KEY, BASE_URL, CURRENCY } from './constants';
+
 export const normalizeNumber = (num: number): number => {
   if (num < 1) {
     return Number(num.toPrecision(5));
   }
   return Number(num.toFixed(3));
+};
+
+export const getPrice = async (name: string): Promise<string | number> => {
+  try {
+    const url = `${BASE_URL}?fsym=${name}&tsyms=${CURRENCY}&api_key=${API_KEY}`;
+    const response = await axios.get(url);
+
+    if (response.data?.Response === 'Error')
+      throw new Error(`Sorry! Coin "${name}" was not found`);
+
+    const currentPrice = normalizeNumber(response.data[CURRENCY]);
+    return currentPrice;
+  } catch (err) {
+    console.log(err);
+    return (err as Error).message;
+  }
 };
